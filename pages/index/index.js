0c856646984bd58bce1ecf327058cc6ad8090ea4
thisang <thisang@foxmail.com>
2019-12-05 08:58:11 +0800

    update: 首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,38 @@
 //index.js
-//获取应用实例
-const app = getApp()
-
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    inputTitle: '',
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  handleInputChange(e) {
+    this.setData({
+      inputTitle: e.detail.value
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+  handleViewEg: function() {
+    wx.navigateTo({
+      url: '../article/article'
+    })
+  },
+  handleGo: function() {
+    if (this.data.inputTitle) {
+      wx.navigateTo({
+        url: '../article/article'
       })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
+      wx.showToast({
+        title: '请输入文章主题',
+        icon: 'none',
+        duration: 2000
       })
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  onLoad: function () {
+  },
+  // 分享页面
+  onShareAppMessage() {
+    wx.showShareMenu({
+      withShareTicket: false
     })
-  }
+  },
 })
