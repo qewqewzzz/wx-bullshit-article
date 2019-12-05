183eca923eff4f049d4986f9037852778ebfce43
thisang <thisang@foxmail.com>
2019-12-05 21:20:57 +0800

    完善分享功能

--- pages/article/article.js
+++ pages/article/article.js
@@ -13,13 +13,6 @@ Page({
       delta: 1
     })
   },
-  handleShare() {
-    wx.showToast({
-      title: 'wait',
-      icon: 'none',
-      duration: 2000
-    })
-  },
   handleGetArticle(title) {
 
     let 论述 = [ 
@@ -244,51 +237,12 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    return {
+      title: '哈哈哈，这篇文章好有趣',
+      path: `/pages/article/article?title=${this.data.title}`
+    }
   }
 })
